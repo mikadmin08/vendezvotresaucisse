dcce6d1617a4a82535b10d196f90ca5035b6bfc3
Axcomma <axelroccat@gmail.com>
2025-11-03 00:45:10 +0100

    features:(homepage): ajoute de la partie haute de la home page (en cours)

--- vendezvotresaucisse/src/Home.tsx
+++ vendezvotresaucisse/src/Home.tsx
@@ -1,7 +1,20 @@
+import { Image } from 'primereact/image';
+
 export const Home = () => {
     return (
-        <div className="container">
-            <p><span className="orange">Maintenance</span> en <span className="orange">cours</span></p>
+        <div className="top-blue-saucisse-container">
+            <h4 className="saucisse-home title">Combien vaut votre Saucisse ?</h4>
+            <div className='saucisse-home hero'>
+
+            <ul className="saucisse-home list">
+                <li><i className="pi pi-check"></i>Évaluation 100% gratuite</li>
+                <li><i className="pi pi-check"></i>Rapide et facile</li>
+                <li><i className="pi pi-check"></i>Obtenez votre prix de vente directement en ligne</li>
+            </ul>
+           <Image style={{position:"relative", left:"30%", scale:"2"}} src="https://content.wirkaufendeinauto.de/static/car_images/wkda-white-car-desktop_optimized.webp" alt="Image" preview width="250" />
+
+            </div>
         </div>
     )
 }
+
--- vendezvotresaucisse/src/main.tsx
+++ vendezvotresaucisse/src/main.tsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { HashRouter } from 'react-router'
 
+import 'primereact/resources/themes/saga-blue/theme.css'
+import 'primereact/resources/primereact.min.css'
+import 'primeicons/primeicons.css'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <HashRouter>
