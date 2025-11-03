c4a059abf1c8c2af1b65a0ee6d6204e8cb9a1313
Michael <m.bourmeau@alphitan.com>
2025-11-03 13:31:52 +0100

    Fix: (header) Correction des liens pour remplacer les boutons + feature: (A propos) Ajout d'un texte contexte

--- vendezvotresaucisse/src/Header.tsx
+++ vendezvotresaucisse/src/Header.tsx
@@ -1,18 +1,30 @@
-import { useNavigate } from "react-router"
+import {useEffect, useState} from "react";
+import { useNavigate, Link } from "react-router"
 import Logo from "./assets/logo.png";
 import "./index.scss";
 
 export const Header = () => {
-
     const navigate = useNavigate();
+    const [screen, setScreen] = useState("");
+
+    const handleScreen = (screen: string) => {
+        navigate(screen);
+        setScreen(screen)
+    }
+
+    useEffect(()=> {
+        const scr = window.location.href.split("/#")[1];
+        setScreen(scr);
+    }, [])
+
     return (
         <div className="header">
-            <img onClick={()=>navigate("/")} src={Logo} alt="logo" />
+            <img onClick={()=>handleScreen("/")} src={Logo} alt="logo" />
             <div className="navigation">
-                <button onClick={()=>navigate("/vendre")}>Vendre saucisse</button>
-                <button onClick={()=>navigate("/estimer")}>Estimer saucisse</button>
-                <button onClick={()=>navigate("/acheter")}>Acheter saucisse</button>
-                <button onClick={()=>navigate("/propos")}>A propos</button>
+                <Link className={`nav ${screen === "/vendre" ? "active" : ""}`} to="vendre" onClick={()=>handleScreen("/vendre")}>Vendre saucisse</Link>
+                <Link className={`nav ${screen === "/estimer" ? "active" : ""}`} to="estimer" onClick={()=>handleScreen("/estimer")}>Estimer saucisse</Link>
+                <Link className={`nav ${screen === "/acheter" ? "active" : ""}`} to="acheter" onClick={()=>handleScreen("/acheter")}>Acheter saucisse</Link>
+                <Link className={`nav ${screen === "/propos" ? "active" : ""}`} to="propos" onClick={()=>handleScreen("/propos")}>A propos</Link>
             </div>
         </div>
     )
--- vendezvotresaucisse/src/Propos.tsx
+++ vendezvotresaucisse/src/Propos.tsx
@@ -4,6 +4,16 @@ import PPTwitch from "./assets/pptwitch.png";
 export const Propos = () => {
     return (
         <div className="container apropos">
+            <section className="context">
+                <h1>Contexte du site</h1>
+                <p>
+                    Ce site est une parodie évidente du site <span>vendezvotrevoiture.fr</span>. 
+                    Cela prends place au sein de la communauté de la streameuse du doux nom de EosCall__, qui permet à ses viewers, 
+                    en échange de points de chaine, de diffuser de fausses pubs. Parmi celles-ci, une en particulier fait référence au site original
+                    remplacant les voitures par des saucisses. Nous avons trouvé ça drôle et voulu rendre réalité le site sur lequel vous êtes présent.
+                </p>
+                <span>Si vous avez aimé le concept et voulez en découvrir plus sur EosCall__, descendez la page pour la découvrir.</span>
+            </section>
             <div className="presentation">
                 <img src={PPTwitch} alt="pptwitch"/>
                 <div className="description">
