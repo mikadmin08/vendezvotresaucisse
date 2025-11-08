214b5364b637986ae3072f7a43f563d343520838
Michael BOURMEAU <michael.bourmeau@outlook.fr>
2025-11-08 10:37:08 +0100

    Fix A Propos + Ajout footer

--- vendezvotresaucisse/src/App.tsx
+++ vendezvotresaucisse/src/App.tsx
@@ -5,18 +5,22 @@ import {Vendre} from "./pages/Vendre/Vendre";
 import { Header } from "./components/Header";
 import { Propos } from "./pages/Propos/Propos";
 import {Home} from "./Home";
+import {Footer} from "./components/Footer";
+import 'primeicons/primeicons.css';
+
 
 function App() {
   return (
     <>
       <Header />
       <Routes>
-        <Route index path="/" element={<Home/>}/>
-        <Route path="/vendre" element={<Vendre/>}/>
-        <Route path="/acheter" element={<Acheter/>}/>
-        <Route path="/estimer" element={<Estimer/>}/>
-        <Route path="/propos" element={<Propos/>}/>
+        <Route index path="/" element={ <Home /> }/>
+        <Route path="/vendre" element={ <Vendre /> }/>
+        <Route path="/acheter" element={ <Acheter /> }/>
+        <Route path="/estimer" element={ <Estimer /> }/>
+        <Route path="/propos" element={ <Propos /> }/>
       </Routes>
+      <Footer/>
     </>
   )
 }
