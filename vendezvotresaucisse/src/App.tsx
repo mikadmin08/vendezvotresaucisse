980b00f1e50f4238f5f043a5f6e3ea021750a362
Axcomma <axelroccat@gmail.com>
2025-11-08 13:35:26 +0100

    feature: add topbtnscroll and saucisse gamification

--- vendezvotresaucisse/src/App.tsx
+++ vendezvotresaucisse/src/App.tsx
@@ -1,26 +1,27 @@
-import {Route, Routes} from "react-router";
+import { Route, Routes } from "react-router";
 import { Acheter } from "./pages/Acheter/Acheter";
 import { Estimer } from "./pages/Estimer/Estimer";
-import {Vendre} from "./pages/Vendre/Vendre";
+import { Vendre } from "./pages/Vendre/Vendre";
 import { Header } from "./components/Header";
 import { Propos } from "./pages/Propos/Propos";
-import {Home} from "./Home";
-import {Footer} from "./components/Footer";
+import { Home } from "./Home";
+import { Footer } from "./components/Footer";
 import 'primeicons/primeicons.css';
-
+import { ScrollTop } from 'primereact/scrolltop';
 
 function App() {
   return (
     <>
       <Header />
+      <ScrollTop />
       <Routes>
-        <Route index path="/" element={ <Home /> }/>
-        <Route path="/vendre" element={ <Vendre /> }/>
-        <Route path="/acheter" element={ <Acheter /> }/>
-        <Route path="/estimer" element={ <Estimer /> }/>
-        <Route path="/propos" element={ <Propos /> }/>
+        <Route index path="/" element={<Home />} />
+        <Route path="/vendre" element={<Vendre />} />
+        <Route path="/acheter" element={<Acheter />} />
+        <Route path="/estimer" element={<Estimer />} />
+        <Route path="/propos" element={<Propos />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </>
   )
 }
