879e84416e451892dad760b4dfd4d13b0ac99185
Michael BOURMEAU <michael.bourmeau@outlook.fr>
2025-11-08 13:41:14 +0100

    Features: Ajout bouton scrollToTop et fix nom du fichier Footer.jsx en Footer.tsx

--- vendezvotresaucisse/src/App.tsx
+++ vendezvotresaucisse/src/App.tsx
@@ -3,16 +3,18 @@ import { Acheter } from "./pages/Acheter/Acheter";
 import { Estimer } from "./pages/Estimer/Estimer";
 import {Vendre} from "./pages/Vendre/Vendre";
 import { Header } from "./components/Header";
+import {Footer} from "./components/Footer";
 import { Propos } from "./pages/Propos/Propos";
 import {Home} from "./Home";
-import {Footer} from "./components/Footer";
 import 'primeicons/primeicons.css';
+import { ScrollTop } from 'primereact/scrolltop';
 
 
 function App() {
   return (
     <>
       <Header />
+      <ScrollTop />
       <Routes>
         <Route index path="/" element={ <Home /> }/>
         <Route path="/vendre" element={ <Vendre /> }/>
