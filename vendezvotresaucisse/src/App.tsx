a19c38b573bff9b077c9e01b1750a45651c2c32f
Michael BOURMEAU <michael.bourmeau@outlook.fr>
2025-11-08 18:47:25 +0100

    Fix Footer + Fin page FAQ

--- vendezvotresaucisse/src/App.tsx
+++ vendezvotresaucisse/src/App.tsx
@@ -1,32 +1,33 @@
-import {Route, Routes} from "react-router";
-import {Acheter} from "./pages/Acheter/Acheter";
-import {Estimer} from "./pages/Estimer/Estimer";
-import {Vendre} from "./pages/Vendre/Vendre";
-import {FAQ} from "./pages/FAQ/FAQ";
-import {Propos} from "./pages/Propos/Propos";
-import {Header} from "./components/Header";
-import {Footer} from "./components/Footer";
-import {Home} from "./Home";
-import 'primeicons/primeicons.css';
-import {ScrollTop} from 'primereact/scrolltop';
-
+import { Route, Routes } from "react-router";
+import { Acheter } from "./pages/Acheter/Acheter";
+import { Estimer } from "./pages/Estimer/Estimer";
+import { Vendre } from "./pages/Vendre/Vendre";
+import { FAQ } from "./pages/FAQ/FAQ";
+import { Propos } from "./pages/Propos/Propos";
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
+import { Home } from "./Home";
+import "primeicons/primeicons.css";
+import { ScrollTop } from "primereact/scrolltop";
 
 function App() {
   return (
     <>
       <Header />
       <ScrollTop />
-      <Routes>
-        <Route index path="/" element={ <Home /> }/>
-        <Route path="/vendre" element={ <Vendre /> }/>
-        <Route path="/acheter" element={ <Acheter /> }/>
-        <Route path="/estimer" element={ <Estimer /> }/>
-        <Route path="/propos" element={ <Propos /> }/>
-        <Route path="/faq" element={ <FAQ /> }/>
-      </Routes>
+      <main>
+        <Routes>
+          <Route index path="/" element={<Home />} />
+          <Route path="/vendre" element={<Vendre />} />
+          <Route path="/acheter" element={<Acheter />} />
+          <Route path="/estimer" element={<Estimer />} />
+          <Route path="/propos" element={<Propos />} />
+          <Route path="/faq" element={<FAQ />} />
+        </Routes>
+      </main>
       <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
